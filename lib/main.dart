745fa7f4c391dfdb1cb38432673a9de0b089ce4d
--replace-all <hishammoonnakal@gmail.com>
2021-06-06 21:29:10 +0530

    format

--- lib/main.dart
+++ lib/main.dart
@@ -2,21 +2,19 @@ import 'package:flutter/material.dart';
 import 'register.dart';
 
 void main() {
-runApp(MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
 // This widget is the root of your application.
-@override
-Widget build(BuildContext context) {
-  return MaterialApp(
-    title: 'Flutter Demo',
-    theme: ThemeData(
-
-      primarySwatch: Colors.blue,
-    ),
-    home: RegisterPageWidget(),
-  );
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: RegisterPageWidget(),
+    );
+  }
 }
-}
-
