96b9d9326dd4bb8144846c32c91aa44f5ed87cef
--replace-all <hishammoonnakal@gmail.com>
2021-06-07 10:36:06 +0530

    calendar

--- lib/calendar.dart
+++ lib/calendar.dart
@@ -1,1 +1,36 @@
+import 'package:flutter/material.dart';
 
+class CalendarWidget extends StatefulWidget {
+  CalendarWidget({Key key}) : super(key: key);
+
+  @override
+  _CalendarWidgetState createState() => _CalendarWidgetState();
+}
+
+class _CalendarWidgetState extends State<CalendarWidget> {
+  final pageViewController = PageController();
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        iconTheme: IconThemeData(color: Colors.white),
+        automaticallyImplyLeading: true,
+        actions: [],
+        centerTitle: true,
+        elevation: 4,
+      ),
+      key: scaffoldKey,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [],
+          ),
+        ),
+      ),
+    );
+  }
+}
